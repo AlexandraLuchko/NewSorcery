20ffa82721a855552c2d886ce36d084e364f4026
Aliaksandra Luchko <lu4ko.aleksandra@gmail.com>
2022-07-01 20:03:46 +0300

    Added ability to drag items from shop to inventory and from inventory to equipment

--- src/components/equipmentCells/Cell/EquipmentCell.jsx
+++ src/components/equipmentCells/Cell/EquipmentCell.jsx
@@ -2,10 +2,20 @@ import * as React from "react";
 import "./equipmentCell.css";
 import { useSelector, useDispatch } from "react-redux";
 import { setEquipment } from "../../../reducers/equipmentCellsReducer";
+import {
+  deleteItemFromShop,
+  setCurrentItem,
+} from "../../../reducers/shopReducer";
+import { deleteItemFromInventory } from "../../../reducers/inventoryReducer";
 
 export default function EquipmentCell({ id, cellData }) {
   const currentItem = useSelector((state) => state.shop.currentItem);
   const dispatch = useDispatch();
+  const equipment = useSelector((state) => state.equipmentCells.equipment);
+
+  function dragStartHandler(e, item) {
+    dispatch(setCurrentItem(item));
+  }
 
   function dragOverHandler(e) {
     e.preventDefault();
@@ -14,6 +24,11 @@ export default function EquipmentCell({ id, cellData }) {
   function dropCaptureHandler(e) {
     e.preventDefault();
     dispatch(setEquipment(id, currentItem));
+    const equipmentCell = equipment.find((item) => item.id == e.target.id);
+    if (equipmentCell.type === currentItem.type) {
+      dispatch(deleteItemFromShop(currentItem));
+      dispatch(deleteItemFromInventory(currentItem, equipment));
+    }
   }
 
   if (cellData.item === null) {
@@ -30,6 +45,7 @@ export default function EquipmentCell({ id, cellData }) {
       <div
         className="cell"
         id={id}
+        onDragStart={(e) => dragStartHandler(e, cellData.item)}
         onDragOver={(e) => dragOverHandler(e)}
         onDrop={(e) => dropCaptureHandler(e)}
       >
--- src/reducers/inventoryReducer.js
+++ src/reducers/inventoryReducer.js
@@ -1,13 +1,27 @@
 const SET_INVENTORY_POPUP_DISPLAY = "SET_INVENTORY_POPUP_DISPLAY";
+const SET_INVENTORY = "SET_INVENTORY";
+const DELETE_ITEM_FROM_INVENTORY = "DELETE_ITEM_FROM_INVENTORY";
 
 const defaultState = {
   popupDisplay: "none",
+  inventory: [],
 };
 
 export default function inventoryReducer(state = defaultState, action) {
   switch (action.type) {
     case SET_INVENTORY_POPUP_DISPLAY:
       return { ...state, popupDisplay: action.payload };
+    case SET_INVENTORY:
+      return {...state, inventory:  [...state.inventory, action.payload]}  
+    case DELETE_ITEM_FROM_INVENTORY:
+      return {
+        ...state,
+        inventory: [
+          ...state.inventory.filter(
+            (item) => item.name !== action.payload.name
+          ),
+        ],
+      }  
     default:
       return state;
   }
@@ -16,4 +30,14 @@ export default function inventoryReducer(state = defaultState, action) {
 export const setInventoryPopupDisplay = (display) => ({
   type: SET_INVENTORY_POPUP_DISPLAY,
   payload: display,
+});
+
+export const setInventory = (payload) => ({
+  type: SET_INVENTORY,
+  payload: payload,
+});
+
+export const deleteItemFromInventory = (item) => ({
+  type: DELETE_ITEM_FROM_INVENTORY,
+  payload: item,
 });
